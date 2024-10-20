c2fe4f6285b80d42fb5ad608d258840c53fd618c
HadeedUlHaq <99009268+HadeedUlHaq@users.noreply.github.com>
2024-10-20 01:34:30 +0100

    final build

--- src/App.vue
+++ src/App.vue
@@ -279,21 +279,21 @@ const projects = [
   {
     title: 'My Family Shopping List',
     description: 'A responsive and feature-rich shopping list web application built from scratch using React.js and Firebase. This app allows users to easily create and manage shopping lists, with personalized item suggestions and real-time updates.',
-    image: '/public/assets/project1.png',
+    image: '/assets/project1.png',
     link: 'https://my-family-shopping-list.pages.dev/app',
     technologies: ['React.js', 'Firebase', 'HTML', 'CSS', 'Cloudflare']
   },
   {
     title: 'Business Upscalers',
     description: 'A professional website developed for Business Upscalers, showcasing services for business growth and development. I designed and implemented a custom WordPress theme from the ground up, focusing on user experience and responsive design.',
-    image: '/public/assets/project2.png',
+    image: '/assets/project2.png',
     link: 'https://businessupscalers.com/',
     technologies: ['WordPress', 'HTML', 'CSS', 'jQuery', 'SEO Optimisation']
   },
   {
     title: 'Affordable Junk Removal Akron',
     description: 'Developed a clean and efficient website for Affordable Junk Removal Akron, focusing on fast load times and an intuitive user interface. I integrated CDN for optimised delivery, implemented lazy loading for images, and ensured robust security protocols.',
-    image: '/public/assets/project3.png',
+    image: '/assets/project3.png',
     link: 'https://www.affordablejunkremovalakron.com/',
     technologies: ['WordPress', 'Cloudflare', 'SSL', 'HTML', 'CSS', 'jQuery']
   }
